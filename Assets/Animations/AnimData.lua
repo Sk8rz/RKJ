bae6f1cf7cf2845a6584c60412f1e85fe1a19b5a
Sk8rz <rad.sk8r.x@gmail.com>
2026-07-20 05:33:01 -0500

    Enhance AnimationData with default state structure
    Added a default state for animations including spawn and combat moves.

--- Assets/Animations/AnimData.lua
+++ Assets/Animations/AnimData.lua
@@ -1,42 +1,80 @@
 local AnimationData = {}
 
 AnimationData.anims = {
-    m1s = {
-        blocked = {
-            [1] = {10469493270},
-            [2] = {10469630950},
-            [3] = {10469639222},
-            [4] = {10469643643}
+    default = {
+        spawn = {
+            blocked = {
+                state = nil,
+                saitama = {15957366251}
+            },
+            replacements = {
+                state = nil,
+                kj = {17325160621}
+            }
         },
-        replacements = {
-            [1] = {17325510002},
-            [2] = {17325513870},
-            [3] = {17325522388},
-            [4] = {17325537719}
-        },
-        downslam = {
-            
-        },
-        variants = {
-            uppercut = {
-                var1 = {
-                    state = nil,
-                    blocked = {
-                        l = {13379003796}
+        m1s = {
+            blocked = {
+                state = nil,
+                [1] = {10469493270},
+                [2] = {10469630950},
+                [3] = {10469639222},
+                [4] = {10469643643}
+            },
+            replacements = {
+                state = nil,
+                [1] = {17325510002},
+                [2] = {17325513870},
+                [3] = {17325522388},
+                [4] = {17325537719}
+            },
+            downslam = {
+                
+            },
+            variants = {
+                uppercut = {
+                    var1 = {
+                        state = nil,
+                        blocked = {
+                            l = {13379003796}
+                        },
+                        replacements = {
+                            r = {10503381238}
+                        }
                     },
-                    replacements = {
-                        r = {10503381238}
-                    }
-                },
-                var2 = {
-                    state = nil,
-                    blocked = {
-                        r = {10503381238}
-                    },
-                    replacements = {
-                        l = {13379003796}
+                    var2 = {
+                        state = nil,
+                        blocked = {
+                            r = {10503381238}
+                        },
+                        replacements = {
+                            l = {13379003796}
+                        }
                     }
                 }
+            }
+        },
+        moves = {
+            blocked = {
+                consecutive = {10466974800}
+            },
+            replacements = {
+                ravage = {16945550029}
+            }
+        },
+        wcombo = {
+            
+        },
+        ult = {
+            
+        }
+    },
+    combat = {
+        moves = {
+            blocked = {
+                consecutive = {10466974800}
+            },
+            replacements = {
+                barrage = {129295156336675}
             }
         }
     }
